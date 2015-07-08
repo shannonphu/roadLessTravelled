1b671fffb74bda02d1f9b47fcd4ba9dbfbae81c2
shannonphu <shannonphu@sbcglobal.net>
2015-07-08 16:55:33 -0700

    add sliding jQuery View More image label

--- roadLessTravelled.js
+++ roadLessTravelled.js
@@ -1,4 +1,17 @@
 $(document).ready(function() {
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 1500, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+
 	var $headerLinks = $('header a');
 	$headerLinks.mouseover(function() {
 		console.log("moused over");
@@ -13,7 +26,28 @@ $(document).ready(function() {
 			marginLeft: "-=30px"
 		}, 'slow');
 	});
-	
+
+	var $viewMore = $('.picLayout aside');
+	var $stateBanner = $('.picLayout');
+	/*$stateBanner.stop(true,true).mouseout(function() {
+		$viewMore.slideUp("slow");
+		//$viewMore.css({ 'display' : 'none' });
+	});
+
+
+	$stateBanner.stop(true,true).mouseover(function() {
+		$viewMore.slideDown("slow");
+				//$viewMore.css({ 'display' : 'block' });
+	});*/
+
+	$stateBanner.hover(
+	    function (event){
+	        $viewMore.slideDown();
+	    },
+	    function (event){
+	        $viewMore.fadeOut().slideUp();
+    });
+
 	/*var $stateBlock = $('.picLayout .stateName');
 	$stateBlock.mouseover(function() {
 		console.log("stateblock mouse over");
